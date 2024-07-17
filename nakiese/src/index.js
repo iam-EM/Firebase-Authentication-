5d6c1caba80eadbc5d75c095458b2a84b2ac7af5
Emmanuel Mukadi <122966190+iam-EM@users.noreply.github.com>
2024-07-17 15:33:09 +0200

    Update index.js
    APIs security

--- nakiese/src/index.js
+++ nakiese/src/index.js
@@ -5,13 +5,13 @@ import { getAuth, createUserWithEmailAndPassword,updateProfile } from 'firebase/
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBa1Vym5RgcUqhLUreRh-bwQDnejROgEa8",
-  authDomain: "nakiesetest.firebaseapp.com",
-  projectId: "nakiesetest",
-  storageBucket: "nakiesetest.appspot.com",
-  messagingSenderId: "142491662773",
-  appId: "1:142491662773:web:aa1266b83607f812c881fc",
-  measurementId: "G-KR8NJMXRSQ"
+ apiKey: "YOUR_API_KEY",
+     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+     projectId: "YOUR_PROJECT_ID",
+     storageBucket: "YOUR_PROJECT_ID.appspot.com",
+     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+     appId: "YOUR_APP_ID",
+     measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 initializeApp(firebaseConfig)
